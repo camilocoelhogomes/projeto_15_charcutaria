9f1e1cabd1b2c67cb2ac4efe19039bf2b798ba2c
thipereira02 <thiago.spereira02@gmail.com>
2021-11-15 11:51:46 -0300

    Adding categories buttons

--- src/components/FrontCover.js
+++ src/components/FrontCover.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import React from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
@@ -8,18 +9,18 @@ import img3 from '../assets/img/3.png';
 import img4 from '../assets/img/4.png';
 
 const FrontCover = () => (
-  <Carousel autoPlay infiniteLoop showThumbs={false}>
+  <Carousel autoPlay infiniteLoop showThumbs={false} showStatus={false}>
     <div>
-      <img alt="" src={img1} />
+      <img alt="Capa" src={img1} />
     </div>
     <div>
-      <img alt="" src={img2} />
+      <img alt="Defumados" src={img2} />
     </div>
     <div>
-      <img alt="" src={img3} />
+      <img alt="Geléias" src={img3} />
     </div>
     <div>
-      <img alt="" src={img4} />
+      <img alt="Molhos" src={img4} />
     </div>
   </Carousel>
 );
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,9 +1,11 @@
+/* eslint-disable array-callback-return */
 import React from 'react';
 import styled from 'styled-components';
 
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import FrontCover from '../../components/FrontCover';
+import ProductContent from '../../components/ProductContent';
 
 const HomePage = () => (
   <>
@@ -11,6 +13,28 @@ const HomePage = () => (
     <Cover>
       <FrontCover />
     </Cover>
+    <Text>
+      <h1>Categorias</h1>
+    </Text>
+    <Buttons>
+      <Option>
+        Defumados
+      </Option>
+      <Option>
+        Geléias
+      </Option>
+      <Option>
+        Molhos
+      </Option>
+    </Buttons>
+    <Text>
+      <h1>Produtos em destaque</h1>
+    </Text>
+    <Highlights>
+      <ProductContent />
+      <ProductContent />
+      <ProductContent />
+    </Highlights>
     <Footer />
   </>
 );
@@ -18,7 +42,52 @@ const HomePage = () => (
 export default HomePage;
 
 const Cover = styled.div`
-    margin: 30px auto;
-    width: 868px;
-    height: 398px;
+  margin: 30px auto;
+  width: 868px;
+  height: 398px;
+`;
+
+const Text = styled.div`
+  margin: 50px auto 0 auto;
+  display: flex;
+  justify-content: center;
+  width: 500px;
+  height: 20px;
+  background-color: #d9d9d9;
+`;
+
+const Highlights = styled.div`
+  width: 80%;
+  margin: 20px auto 0 auto; 
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Buttons = styled.div`
+  width: 80%;
+  margin: 40px auto 60px auto; 
+  display: flex;
+  justify-content: space-around;
+`;
+
+const Option = styled.button`
+  height: 90px;
+  width: 300px;
+  font-size: 20px;
+  color: #FFF;
+  font-family: 'RalewaySemiBold';
+  border-radius: 5px;
+  border-color: #b30b0b;
+  background: linear-gradient(to bottom, #b30b0b 5%,#fca8a1 100%);
+  box-shadow: inset 0px 1px 0px 0px #fff4f3;
+  text-shadow: 0px 1px 0px #000;
+
+  :hover{
+    background: linear-gradient(to bottom, #fca8a1 5%, #b30b0b 100%);
+  }
+
+  :active{
+    position: relative;
+    top: 2px;
+  }
 `;
